7d634b19722a602eb23974657a0a5b379fc3ab71
rusgyn <rusgynmagsanay@gmail.com>
2024-05-30 00:14:06 -0400

    Implement sumItems function and return the sum of the elements in a given array.

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+/**
+ * Make sumItems a function that sums all the numbers in an array. So if you input the array [1, 2, 3, 4, 5], the function will return 15. This will be your base case.
+ * @param {*} array 
+ */
+
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  for (let i of array) {
+    if (Array.isArray(i)) {
+      sum += sumItems(i);
+    } else {
+      sum += i;
+    }
+  }
+  return sum;
 }
 
 module.exports = sumItems;
